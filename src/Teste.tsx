a271abcab38109d1466f0cfbf70e5662355e6527
cieliocas <francielioevangelista@hotmail.com>
2025-01-21 05:48:26 -0300

    add styled button component to Teste and update rendering

--- src/Teste.tsx
+++ src/Teste.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
+type BotaoProps = {
+  princopal: boolean
+}
+
+const Botao = styled.button`
+  background-color: ${props => (props.principal ? 'green' : 'blue')};
+`
+
 function Teste() {
-  return <div>Olá</div>
+  return <Botao principal>Clique aqui</Botao>
 }
 
 export default Teste
