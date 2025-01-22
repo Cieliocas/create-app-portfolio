c66fb60ec0b21472d311180ab45507b1cee69d03
cieliocas <francielioevangelista@hotmail.com>
2025-01-22 06:41:17 -0300

    add Sobre component and integrate it into App with Titulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import EstiloGlobal from './styles.ts'
 import Sidebar from './containers/Sidebar/index.tsx'
+import Sobre from './containers/Sobre/index.tsx'
 
 function App() {
   return (
     <>
       <EstiloGlobal />
       <Sidebar />
+      <main>
+        <Sobre />
+        <section>
+          Projetos
+        </section>
+      </main>
     </>
   )
 }
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,11 @@
+import Titulo from "../../components/Titulo";
+
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre</Titulo>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, unde sapiente assumenda provident, ipsum ullam perferendis tempora laudantium temporibus quae nemo quia totam. Aut tenetur excepturi ipsam dolorum aliquam vitae?</p>
+  </section>
+)
+
+export default Sobre
