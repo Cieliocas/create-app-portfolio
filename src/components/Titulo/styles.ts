ba81725f11f100f25a9eff159c9a148fcffcff3f
cieliocas <francielioevangelista@hotmail.com>
2025-01-23 04:03:45 -0300

    refactor Titulo styles to import Props from the correct file and update font-size to use dynamic value

--- src/components/Titulo/styles.ts
+++ src/components/Titulo/styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
-import { Props } from '.';
+import { Props } from './index.tsx';
 
 export const Titulo = styled.h3<Props>`
   color: #282A35;
-  font-size: 14px;
+  font-size: ${props => props.fontSize}px;
   font-weight: bold;
 `;
 
+// font-size: ${props => props.fontSize ? props.fontSize + 'px' : '14px'};
