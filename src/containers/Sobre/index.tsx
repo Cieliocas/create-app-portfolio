9c51f24a5aa74f8e8bafe8e37af9bf79b104dfc7
cieliocas <francielioevangelista@hotmail.com>
2025-01-23 04:33:23 -0300

    add GitHub stats images to Sobre container

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -6,6 +6,10 @@ const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
     <Paragrafo tipo="principal">Olá, meu nome é Cielio, sou estudante de Ciência da Computação e estou aprendendo a programar.</Paragrafo>
+    <div>
+    <img src="https://github-readme-stats.vercel.app/api?username=cieliocas&show_icons=true&theme=dark#gh-dark-mode-only&include_all_commits=true&count_private=true"/>
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=cieliocas&layout=compact&langs_count=7&&theme=dark#gh-dark-mode-only"/>
+    </div>
   </section>
 )
 
