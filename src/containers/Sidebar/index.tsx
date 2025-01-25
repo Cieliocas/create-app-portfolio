735c97c241ab4a52a5a4b7434eb0facf958ccab0
cieliocas <francielioevangelista@hotmail.com>
2025-01-25 09:26:44 -0300

    add fontSize prop to Paragrafo component; update Sidebar to display username and additional info

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,12 +1,14 @@
 import Avatar from "../../components/Avatar/index.tsx"
 import Titulo from "../../components/Titulo/index.tsx"
+import Paragrafo from "../../components/Paragrafo/index.tsx"
 import React from "react"
 
 
 const Sidebar = () => (
   <aside>
     <Avatar />
-    <Titulo fontSize={20}>Cielio Castro</Titulo>
+    <Titulo fontSize={20}>Franciélio Castro</Titulo>
+    <Paragrafo tipo="secundario" fontSize={16}>cieliocas</Paragrafo>
   </aside>
 )
 
