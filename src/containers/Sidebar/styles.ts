95e9eae85063779819b055715a4da9f0e1907545
cieliocas <francielioevangelista@hotmail.com>
2025-01-25 09:45:21 -0300

    add multiple Projeto components to Projetos container; update BotaoTema styles for improved appearance

--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -8,16 +8,16 @@ export const Descricao = styled(P)`
 `
 
 export const BotaoTema = styled.button`
-  width: 100%;
-  padding: 12px;
-  border-radius: 8px;
-  font-size: 16px;
+  padding: 10px;
+  border-radius: 12px;
+  font-size: 12px;
   transition: background-color 0.3s;
   cursor: pointer;
   background-color: black;
   color: #fff;
   border-color: #fff;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  font-weight: bold;
 
   &:hover {
     background-color:rgb(255, 255, 255);
